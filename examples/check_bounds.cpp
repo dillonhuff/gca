b3369a459b41d29b19b28e4097ca6f2f7399ecc6
Dillon Huff <dillonhuff@gmail.com>
2015-12-20 22:00:08 -0800

    Added better instructions for building examples

--- examples/check_bounds.cpp
+++ examples/check_bounds.cpp
@@ -4,8 +4,21 @@
 using namespace gca;
 using namespace std;
 
-// Test file:
-// /Users/dillon/CppWorkspace/gca/test/test_1.txt
+/* 
+
+   Bounds checker for GCODE programs with absolute coordinates.
+
+   Test file: /Users/dillon/CppWorkspace/gca/test/test_1.txt
+
+   To build the program
+   ( from top level directory, e.g. /Users/dillon/CppWorkspace/gca/ )
+   and run it on test file:
+   cd /Users/dillon/CppWorkspace/gca/
+   make clean
+   make check-bounds-example -j
+   ./check-bounds-example /Users/dillon/CppWorkspace/gca/test/test_1.txt
+
+ */
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -15,7 +28,14 @@ int main(int argc, char* argv[]) {
   string s(argv[1]);
   context c;
   gprog* p = read_file(c, s);
-  bounds_checker b(0, 9, -20, -10, 0.0, 2.0);
+  // Adjust these to whatever boudns you would like to check
+  int x_min = 0;
+  int x_max = 9;
+  int y_min = -20;
+  int y_max = -10;
+  int z_min = 0.0;
+  int z_max = 2.0;
+  bounds_checker b(x_min, x_max, y_min, y_max, z_min, z_max);
   b.check(cout, p);
   return 0;
 }
