7ed6512882f9faf4c82c1cac0dd30e098bf6dde7
Dillon Huff <dillonhuff@gmail.com>
2016-01-05 01:04:32 -0800

    feed_changer is now a state transformation

--- src/feed_changer.h
+++ src/feed_changer.h
@@ -1,38 +1,64 @@
 #ifndef GCA_FEED_CHANGER_H
 #define GCA_FEED_CHANGER_H
 
+#include "basic_states.h"
 #include "context.h"
+#include "pass.h"
+
+#define GCA_FEED_CHANGER_STATE 2001
 
 namespace gca {
 
-  class feed_changer {
+  class feed_changer_state : public per_instr_state {
   protected:
     double initial_feedrate, new_feedrate;
-    
+    context& c;
+
   public:
-    feed_changer(double initial_feedratep, double new_feedratep) {
+    gprog* p;
+    
+  feed_changer_state(context& cp,
+		     pass* tp,
+		     double initial_feedratep,
+		     double new_feedratep) :
+    c(cp) {
+      t = tp;
       initial_feedrate = initial_feedratep;
       new_feedrate = new_feedratep;
+      p = c.mk_gprog();
+    }
+
+    void update_default(instr* ist) {
+      p->push_back(ist);
+    }
+
+    void update_G1(instr* ist) {
+      point kp = ist->pos();
+      orientation orient = ist->is_abs() ? GCA_ABSOLUTE : GCA_RELATIVE;
+      p->push_back(c.mk_G1(kp.x, kp.y, kp.z, new_feedrate, orient));
+    }
+
+  };
+
+  class feed_changer : public pass {
+  protected:
+    current_instr_state cis;
+    feed_changer_state feed_s;
+
+  public:
+  feed_changer(context& c, double initial_feedratep, double new_feedratep) :
+    cis(this),
+      feed_s(c, this, initial_feedratep, new_feedratep) {
+      states[GCA_INSTR_STATE] = &cis;
+      states[GCA_FEED_CHANGER_STATE] = &feed_s;
     }
 
     virtual gprog* apply(context& c, gprog* p) {
-      gprog* n = c.mk_gprog();
-      for (ilist::iterator it = p->begin();
-	   it != p->end(); ++it) {
-	instr k = **it;
-	if (k.is_G1()) {
-	  point kp = k.pos();
-	  orientation orient = k.is_abs() ? GCA_ABSOLUTE : GCA_RELATIVE;
-	  n->push_back(c.mk_G1(kp.x, kp.y, kp.z, new_feedrate, orient));
-	} else {
-	  n->push_back(&k);
-	}
-      }
-      return n;
+      exec(p);
+      return feed_s.p;
     }
   };
 
 }
 
 #endif
-
--- test/transformer_tests.cpp
+++ test/transformer_tests.cpp
@@ -3,10 +3,22 @@
 #include "context.h"
 #include "feed_changer.h"
 #include "g0_filter.h"
+#include "parser.h"
 #include "rel_to_abs.h"
 #include "tiler.h"
 
 namespace gca {
+
+  TEST_CASE("Feed changer with G0") {
+    context c;
+    gprog* p = parse_gprog(c, "G91 G0 X1.5 G1 F2 X2.0 Y3.0 Z5.5");
+    double initial_feedrate = 2.0;
+    double new_feedrate = 5.0;
+    feed_changer f(c, initial_feedrate, new_feedrate);
+    gprog* n = f.apply(c, p);
+    gprog* correct = parse_gprog(c, "G91 G0 X1.5 G1 F5 X2.0 Y3.0 Z5.5");
+    REQUIRE(*n == *correct);
+  }
   
   TEST_CASE("Feed changer") {
     context c;
@@ -14,7 +26,7 @@ namespace gca {
     double initial_feedrate = 1.0;
     p->push_back(c.mk_G1(1.0, 1.0, 1.0, initial_feedrate));
     double new_feedrate = 4.0;
-    feed_changer f(initial_feedrate, new_feedrate);
+    feed_changer f(c, initial_feedrate, new_feedrate);
     gprog* n = f.apply(c, p);
     gprog* correct = c.mk_gprog();
     correct->push_back(c.mk_G1(1.0, 1.0, 1.0, new_feedrate));
@@ -27,7 +39,7 @@ namespace gca {
     double initial_feedrate = 1.0;
     p->push_back(c.mk_G1(1.0, 1.0, 1.0, initial_feedrate, GCA_RELATIVE));
     double new_feedrate = 4.0;
-    feed_changer f(initial_feedrate, new_feedrate);
+    feed_changer f(c, initial_feedrate, new_feedrate);
     gprog* n = f.apply(c, p);
     gprog* correct = c.mk_gprog();
     correct->push_back(c.mk_G1(1.0, 1.0, 1.0, new_feedrate, GCA_RELATIVE));
