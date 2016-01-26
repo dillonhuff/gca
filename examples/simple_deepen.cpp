89d2c4730a57240aab52543744432af278def957
Dillon Huff <dillonhuff@gmail.com>
2016-01-26 13:24:32 -0800

    Now use push depth parameter to deal with drag knife movement

--- examples/simple_deepen.cpp
+++ examples/simple_deepen.cpp
@@ -15,17 +15,15 @@ double get_z(move_instr* mi) {
   return z_lit->v;
 }
 
-double deepen_z(double z, double old_depth, double new_depth) {
+double deepen_z(double z, double push_depth, double old_depth, double new_depth) {
   assert(new_depth > old_depth);
   double diff = new_depth - old_depth;
   double deeper_z;
-  if (z > old_depth) {
+  if (z > old_depth || within_eps(z, old_depth - push_depth)) {
     deeper_z = z + diff;
-  } else if (old_depth >= z && z >= old_depth*(3.0/4.0)) {
-    deeper_z = z + diff;
-  } else if (old_depth*(3.0/4.0) >= z && z >= old_depth*(1.0/4.0)) {
+  } else if (old_depth >= z && z > 0) {
     deeper_z = z + diff/2.0;
-  } else if (z == 0.0) {//within_eps(z, 0.8) || within_eps(z, 0.0)) {
+  } else if (z == 0.0) {
     deeper_z = z;
   } else {
     cout << "Unsupported z = " << z << endl;
@@ -36,7 +34,7 @@ double deepen_z(double z, double old_depth, double new_depth) {
   return deeper_z;
 }
 
-gprog* deepen(context& c, gprog* p, double old_depth, double new_depth) {
+gprog* deepen(context& c, gprog* p, double push_depth, double old_depth, double new_depth) {
   gprog* r = c.mk_gprog();
   for (int i = 0; i < p->size(); i++) {
     instr* is = (*p)[i];
@@ -44,7 +42,7 @@ gprog* deepen(context& c, gprog* p, double old_depth, double new_depth) {
       move_instr* mi = static_cast<move_instr*>(is);
       if (!mi->get_z()->is_omitted()) {
 	double z = get_z(mi);
-	double deeper_z = deepen_z(z, old_depth, new_depth);
+	double deeper_z = deepen_z(z, push_depth, old_depth, new_depth);
 	r->push_back(c.mk_G0(mi->get_x(), mi->get_y(), c.mk_lit(deeper_z)));
       } else {
 	r->push_back(mi);
@@ -53,7 +51,7 @@ gprog* deepen(context& c, gprog* p, double old_depth, double new_depth) {
       move_instr* mi = static_cast<move_instr*>(is);
       if (!mi->get_z()->is_omitted()) {
 	double z = get_z(mi);
-	double deeper_z = deepen_z(z, old_depth, new_depth);
+	double deeper_z = deepen_z(z, push_depth, old_depth, new_depth);
 	r->push_back(c.mk_G1(mi->get_x(), mi->get_y(), c.mk_lit(deeper_z), mi->feed_rate));
       } else {
 	r->push_back(mi);
@@ -70,16 +68,17 @@ gprog* deepen(context& c, gprog* p, double old_depth, double new_depth) {
 }
 
 int main(int argc, char** argv) {
-  if (argc != 4) {
-    cout << "Usage: gdiff <old_depth> <new_depth> <gcode_file_path>" << endl;
+  if (argc != 5) {
+    cout << "Usage: gdiff <old depth> <new depth> <drag knife push depth> <gcode file path>" << endl;
     return 0;
   }
   double old_depth = stod(argv[1]);
   double new_depth = stod(argv[2]);
-  string file = argv[3];
+  double push_depth = stod(argv[3]);
+  string file = argv[4];
   context c;
   gprog* p = read_file(c, file);
-  gprog* r = deepen(c, p, old_depth, new_depth);
+  gprog* r = deepen(c, p, push_depth, old_depth, new_depth);
   cout << *r;
   return 0;
 }
