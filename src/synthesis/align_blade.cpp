ebc863988a0def5331c3448831dee449a69a5186
Dillon Huff <dillonhuff@gmail.com>
2016-01-30 15:23:51 -0800

    Pulled circle code generation out into its own file

--- src/synthesis/align_blade.cpp
+++ src/synthesis/align_blade.cpp
@@ -32,6 +32,26 @@ namespace gca {
     return circular_arc(circle_start, circle_end, circle_start_off);
   }
 
+  move_instr* circular_arc_to_gcode(circular_arc ca) {
+    // Computing signed angle
+    point sv = -1 * ca.start_offset;
+    point ev = ca.end - ca.center();
+    double angle = atan2(ev.y, ev.x) - atan2(sv.y, sv.x);
+    cout << "Angle between = " << angle << endl;
+    move_instr* circle_move_instr;
+    if (angle < 0) {
+      circle_move_instr = mk_G2(mk_lit(ca.end.x), mk_lit(ca.end.y), mk_omitted(),
+				mk_lit(ca.start_offset.x), mk_lit(ca.start_offset.y), mk_omitted(),
+				mk_omitted());
+    } else {
+      circle_move_instr = mk_G3(mk_lit(ca.end.x), mk_lit(ca.end.y), mk_omitted(),
+				mk_lit(ca.start_offset.x), mk_lit(ca.start_offset.y), mk_omitted(),
+				mk_omitted());
+
+    }
+    return circle_move_instr;
+  }
+
   void from_to_with_G0_drag_knife(double safe_height,
 				  double align_depth,
 				  gprog* p,
@@ -46,22 +66,7 @@ namespace gca {
     circular_arc ca = align_coords(next_orient, next_pos_xy, last_orient, r);
     instr* move_to_c_pos_instr = mk_G0(ca.start.x, ca.start.y, safe_height);
     instr* push_down_instr = mk_G1(ca.start.x, ca.start.y, align_depth, mk_omitted());
-    // Computing signed angle
-    point sv = -1 * ca.start_offset;
-    point ev = next_pos - ca.center();
-    double angle = atan2(ev.y, ev.x) - atan2(sv.y, sv.x);
-    instr* circle_move_instr;
-    cout << "Angle between = " << angle << endl;
-    if (angle < 0) {
-      circle_move_instr = mk_G2(mk_lit(next_pos.x), mk_lit(next_pos.y), mk_omitted(),
-				mk_lit(ca.start_offset.x), mk_lit(ca.start_offset.y), mk_omitted(),
-				mk_omitted());
-    } else {
-      circle_move_instr = mk_G3(mk_lit(next_pos.x), mk_lit(next_pos.y), mk_omitted(),
-				mk_lit(ca.start_offset.x), mk_lit(ca.start_offset.y), mk_omitted(),
-				mk_omitted());
-
-    }
+    instr* circle_move_instr = circular_arc_to_gcode(ca);
     instr* final_push_down_instr = mk_G1(next_pos.x, next_pos.y, next_pos.z, mk_omitted());
     p->push_back(pull_up_instr);
     p->push_back(move_to_c_pos_instr);
