9f8fa7b37d03540d477809d86fd0bdc37ab6b40e
Dillon Huff <dillonhuff@gmail.com>
2016-02-18 17:59:55 -0500

    Now have automatic platform detection in system/settings.cpp

--- src/system/settings.cpp
+++ src/system/settings.cpp
@@ -1,5 +1,13 @@
 #include "system/settings.h"
 
 namespace gca {
+
+#ifdef __APPLE__
   string project_path = "/Users/dillon/CppWorkspace/";
+#elif __linux__
+  string project_path = "/home/probotix/CppWorkspace/";
+#else
+# error unknown compiler
+#endif
+
 }
