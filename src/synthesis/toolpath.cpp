d63a2c33601639fbef0499fba5c6df034c910246
Dillon Huff <dillonhuff@gmail.com>
2016-02-20 15:46:34 -0800

    Eliminated some old, unneeded code

--- src/synthesis/toolpath.cpp
+++ src/synthesis/toolpath.cpp
@@ -5,42 +5,6 @@
 
 namespace gca {
 
-  struct continuous_checker {
-    double max_orientation_change;
-    
-    continuous_checker(double pmax_orientation_change) :
-      max_orientation_change(pmax_orientation_change) {}
-    
-    bool operator()(const cut* next_cut, const cut* last_cut) {
-      if (!within_eps(last_cut->end, next_cut->start)) {
-      	return false;;
-      }
-      double theta = angle_between(last_cut->final_orient(), next_cut->initial_orient());
-      return theta <= max_orientation_change;
-    }
-  };
-  
-  void collect_adjacent_cuts(const vector<cut*>& cuts,
-			     vector<cut*>& cut_group,
-			     unsigned i,
-			     double max_orientation_change) {
-    // cut_group.push_back(cuts[i]);
-    // if (cuts.size() == i - 1) {
-    //   return;
-    // }
-    // unsigned j = i;
-    // not_continuous_checker cts(max_orientation_change);
-    // while (j < cuts.size() - 1) {
-    //   cut* last_cut = cuts[j];
-    //   cut* next_cut = cuts[j + 1];
-    //   if (cts(*last_cut, *next_cut)) {
-    // 	break;
-    //   }
-    //   cut_group.push_back(cuts[j]);
-    //   j++;
-    // }
-  }
-
   struct cut_appender {
     double max_orientation_change;
     vector<cut_group>& cut_groups;
