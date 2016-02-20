700de72dd785a2b5a7839589ade12999a3a45278
Dillon Huff <dillonhuff@gmail.com>
2016-02-20 14:35:49 -0800

    Added more cut group tests

--- test/toolpath_tests.cpp
+++ test/toolpath_tests.cpp
@@ -14,29 +14,56 @@ namespace gca {
   }
 
 
-  TEST_CASE("Several non-adjacent lines") {
+  TEST_CASE("Cut groups for lines") {
     arena_allocator a;
     set_system_allocator(&a);
 
     vector<cut*> cuts;
-    cuts.push_back(mk_linear_cut(point(0, 0, 0), point(1, 0, 0)));
-    cuts.push_back(mk_linear_cut(point(1, 0, 0), point(1, 1, 0)));
-    
     vector<cut_group> cgs;
 
-    SECTION("No cuts lost in cut group creation") {
-      vector<cut_group> cgs;
-      group_adjacent_cuts(cuts, cgs, 30.0);
-      unsigned nc = num_cuts(cgs);
-      unsigned correct = cuts.size();
-      REQUIRE(nc == correct);
+    SECTION("Several non-continuous lines") {
+      cuts.push_back(mk_linear_cut(point(0, 0, 0), point(1, 0, 0)));
+      cuts.push_back(mk_linear_cut(point(1, 0, 0), point(1, 1, 0)));
+      cuts.push_back(mk_linear_cut(point(2, 0, 0), point(3, 2, 0)));
+
+      SECTION("No cuts lost in cut group creation") {
+	group_adjacent_cuts(cuts, cgs, 30.0);
+	unsigned nc = num_cuts(cgs);
+	unsigned correct = cuts.size();
+	REQUIRE(nc == correct);
+      }
+
+      SECTION("Each cut is its own cut group") {
+	group_adjacent_cuts(cuts, cgs, 30.0);
+	unsigned correct = cuts.size();
+	REQUIRE(cgs.size() == correct);
+      }
     }
 
-    SECTION("Each cut is its own cut group") {
-      vector<cut_group> cgs;
+    SECTION("One cut group with multiple cuts") {
+      cuts.push_back(mk_linear_cut(point(0, 0, 0), point(1, 0, 0)));
+      cuts.push_back(mk_linear_cut(point(1, 0, 0), point(2, 0, 0)));
+
       group_adjacent_cuts(cuts, cgs, 30.0);
-      unsigned correct = cuts.size();
-      REQUIRE(cgs.size() == correct);
+      REQUIRE(cgs.size() == 1);
+    }
+
+    SECTION("2 distinct adjacent groups") {
+      cuts.push_back(mk_linear_cut(point(0, 0, 0), point(1, 0, 0)));
+      cuts.push_back(mk_linear_cut(point(1, 0, 0), point(2, 0.05, 0)));
+      cuts.push_back(mk_linear_cut(point(2, 0, 0), point(3, 2, 0)));
+      
+      group_adjacent_cuts(cuts, cgs, 30.0);
+
+      SECTION("No cuts lost in cut group creation") {
+	unsigned nc = num_cuts(cgs);
+	unsigned correct = cuts.size();
+	REQUIRE(nc == correct);
+      }
+
+      SECTION("Split into 2 groups") {
+	REQUIRE(cgs.size() == 2);
+      }
     }
   }
 }
