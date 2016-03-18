75b6072e5ca2f891724be6e603f0ef70ef6e5243
Dillon Huff <dillonhuff@gmail.com>
2016-03-17 20:50:44 -0700

    Removed abs -> rel and rel -> abs as they had never been used and were not built to deal with what I now understand is realistic GCODE

--- test/transformer_tests.cpp
+++ test/transformer_tests.cpp
@@ -1,10 +1,8 @@
 #include "catch.hpp"
 #include "core/parser.h"
 #include "core/lexer.h"
-#include "transformers/abs_to_rel.h"
 #include "transformers/feed_changer.h"
 #include "transformers/g0_filter.h"
-#include "transformers/rel_to_abs.h"
 
 namespace gca {
 
@@ -101,74 +99,5 @@ namespace gca {
       correct->push_back(g1_instr::make(2, 3, 3));
       REQUIRE(*filter_G0_moves(p) == *correct);
     }
-  }
-  
-  TEST_CASE("abs -> rel conversion") {
-    arena_allocator a;
-    set_system_allocator(&a);    
-    gprog* p = gprog::make();
-    gprog* r = gprog::make();
-    gprog* correct = gprog::make();
-    abs_to_rel f(GCA_ABSOLUTE);
-
-    SECTION("abs -> rel 1 instruction is the same") {
-      p->push_back(g0_instr::make(1.0, 1.0, 1.0));
-      correct->push_back(g0_instr::make(1.0, 1.0, 1.0));
-      r = f.apply(p);
-      REQUIRE(*r == *correct);
-    }
-
-    SECTION("abs -> rel 1 m instruction is the same") {
-      p->push_back(m2_instr::make());
-      correct->push_back(m2_instr::make());
-      r = f.apply(p);
-      REQUIRE(*r == *correct);
-    }
-    
-    SECTION("abs -> rel 2 instructions") {
-      p->push_back(g1_instr::make(1.0, 0, 0));
-      p->push_back(g0_instr::make(2.0, 3.5, 8));
-      correct->push_back(g1_instr::make(1.0, 0, 0, 1.0));
-      correct->push_back(g0_instr::make(1.0, 3.5, 8));
-      r = f.apply(p);
-      REQUIRE(*r == *correct);
-    }
-    
-  }
-
-  TEST_CASE("rel -> abs conversion") {
-    
-    arena_allocator a;
-    set_system_allocator(&a);    
-    gprog* p = gprog::make();
-    gprog* r;
-    gprog* correct = gprog::make();
-    rel_to_abs f(GCA_ABSOLUTE);
-
-    SECTION("One M2 instruction is the same") {
-      p->push_back(g91_instr::make());
-      p->push_back(m2_instr::make());
-      correct->push_back(m2_instr::make());
-      r = f.apply(p);
-      REQUIRE(*r == *correct);
-    }
-
-    SECTION("One G0 instruction is the same") {
-      p->push_back(g91_instr::make());
-      p->push_back(g0_instr::make(point(1.0, 2.0, 3.0)));
-      correct->push_back(g0_instr::make(point(1.0, 2.0, 3.0)));
-      r = f.apply(p);
-      REQUIRE(*r == *correct);
-    }
-
-    SECTION("Two instructions instructions") {
-      p->push_back(g91_instr::make());
-      p->push_back(g0_instr::make(point(1.0, 2.0, 3.0)));
-      p->push_back(g1_instr::make(-2.0, 2.0, -10.0, 2.5));
-      correct->push_back(g0_instr::make(point(1.0, 2.0, 3.0)));
-      correct->push_back(g1_instr::make(-1.0, 4.0, -7.0, 2.5));
-      r = f.apply(p);
-      REQUIRE(*r == *correct);
-    }    
   }
 }
