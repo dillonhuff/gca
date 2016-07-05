6c192096d9aa87b8ae3c871fccbd622d3014774c
Dillon Huff <dillonhuff@gmail.com>
2016-07-05 16:01:55 -0700

    More deletions and modifications in fixture_analysis_tests

--- test/fixture_analysis_tests.cpp
+++ test/fixture_analysis_tests.cpp
@@ -31,31 +31,21 @@ namespace gca {
 
     vice test_vice = emco_vice(point(1.2, -4.4, 3.3));
     fixtures fixes(test_vice);
+    tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
+    vector<tool> tools{t1};
     workpiece workpiece_dims(4.0, 4.0, 3.98, ALUMINUM);
 
     auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/TaperedTopSeveralVerticals.stl", 0.001);
 
     auto outer_surfs = outer_surfaces(mesh);
-    auto aligned_workpiece = align_workpiece(outer_surfs, workpiece_dims);
-    classify_part_surfaces(outer_surfs, aligned_workpiece);
-    auto surfs_to_cut = surfaces_to_cut(mesh, outer_surfs);
 
     SECTION("14 outer surfaces") {
       REQUIRE(outer_surfs.size() == 14);
     }
 
-    SECTION("8 surfaces to cut") {
-      REQUIRE(surfs_to_cut.size() == 8);
-    }
-
-    SECTION("3 setups") {
-      vector<fixture> all_orients =
-	all_stable_fixtures(outer_surfs, fixes);
-
-      surface_map orients =
-	pick_orientations(mesh, surfs_to_cut, all_orients);
-
-      REQUIRE(orients.size() == 3);
+    SECTION("9 setups") {
+      fixture_plan p = make_fixture_plan(mesh, fixes, tools, workpiece_dims);
+      REQUIRE(p.fixtures().size() == 9);
     }
   }
   
@@ -65,49 +55,26 @@ namespace gca {
 
     vice test_vice = emco_vice(point(-0.8, -4.4, -3.3));
     fixtures fixes(test_vice);
+    tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
+    vector<tool> tools{t1};
     workpiece workpiece_dims(3.5, 2.5, 2.3, BRASS);
+
     auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/TaperedExtrudedTopSide.stl", 0.001);
 
     auto outer_surfs = outer_surfaces(mesh);
-    auto aligned_workpiece = align_workpiece(outer_surfs, workpiece_dims);
-    classify_part_surfaces(outer_surfs, aligned_workpiece);
-    auto surfs_to_cut = surfaces_to_cut(mesh, outer_surfs);
 
     SECTION("6 outer surfaces") {
       REQUIRE(outer_surfs.size() == 6);
     }
 
-    SECTION("2 setups") {
-      vector<fixture> all_orients =
-	all_stable_fixtures(outer_surfs, fixes);
+    SECTION("8 setups") {
+      fixture_plan p = make_fixture_plan(mesh, fixes, tools, workpiece_dims);
 
-      surface_map orients =
-	pick_orientations(mesh, surfs_to_cut, all_orients);
-
-      REQUIRE(orients.size() == 2);
+      REQUIRE(p.fixtures().size() == 8);
 
       // No use of base plates
-      for (auto fixture : orients) {
-	REQUIRE(!(all_orients[fixture.first].v.has_protective_base_plate()));
-      }
-    }
-
-    SECTION("each orientation has 1 connected component") {
-      vector<fixture> all_orients =
-	all_stable_fixtures(outer_surfs, fixes);
-
-      surface_map orients =
-	pick_orientations(mesh, surfs_to_cut, all_orients);
-      
-      for (auto p : orients) {
-	auto surface_inds = p.second;
-	auto connected_comps =
-	  connected_components_by(surface_inds,
-				  [surfs_to_cut](const unsigned i, const unsigned j)
-				  {
-				    return surfaces_share_edge(i, j, surfs_to_cut);
-				  });
-	REQUIRE(connected_comps.size() == 1);
+      for (auto fixture : p.fixtures()) {
+	REQUIRE(!(fixture.v.has_protective_base_plate()));
       }
     }
   }
@@ -117,65 +84,77 @@ namespace gca {
     set_system_allocator(&a);
     vice test_vice = emco_vice(point(1.8, 4.2, 3.3));
     fixtures fixes(test_vice);
+    tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
+    vector<tool> tools{t1};
     workpiece workpiece_dims(2.0, 2.0, 2.0, ACETAL);
+
     auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/LittleHouse.stl", 0.001);
 
     auto outer_surfs = outer_surfaces(mesh);
-    auto aligned_workpiece = align_workpiece(outer_surfs, workpiece_dims);
-    classify_part_surfaces(outer_surfs, aligned_workpiece);
-    auto surfs_to_cut = surfaces_to_cut(mesh, outer_surfs);
+    // auto aligned_workpiece = align_workpiece(outer_surfs, workpiece_dims);
+    // classify_part_surfaces(outer_surfs, aligned_workpiece);
+    // auto surfs_to_cut = surfaces_to_cut(mesh, outer_surfs);
 
-    SECTION("4 surfaces to cut") {
-      REQUIRE(surfs_to_cut.size() == 4);
-    }
+    // SECTION("4 surfaces to cut") {
+    //   REQUIRE(surfs_to_cut.size() == 4);
+    // }
     
     SECTION("10 outer surfaces") {
       REQUIRE(outer_surfs.size() == 10);
     }
 
-    SECTION("6 surfaces that are always stable") {
-      unsigned num_stable = 0;
-      for (auto s : outer_surfs) {
-	if (s.is_SA()) {
-	  num_stable++;
-	}
-      }
-      REQUIRE(num_stable == 6);
-    }
+    // SECTION("6 surfaces that are always stable") {
+    //   unsigned num_stable = 0;
+    //   for (auto s : outer_surfs) {
+    // 	if (s.is_SA()) {
+    // 	  num_stable++;
+    // 	}
+    //   }
+    //   REQUIRE(num_stable == 6);
+    // }
 
-    SECTION("1 setup") {
-      vector<fixture> all_orients =
-	all_stable_fixtures(outer_surfs, fixes);
+    SECTION("7 setups") {
+      fixture_plan p = make_fixture_plan(mesh, fixes, tools, workpiece_dims);
 
-      surface_map orients =
-	pick_orientations(mesh, surfs_to_cut, all_orients);
-
-      REQUIRE(orients.size() == 1);
+      REQUIRE(p.fixtures().size() == 7);
 
       // No use of base plates
-      for (auto fixture : orients) {
-	REQUIRE(!(all_orients[fixture.first].v.has_protective_base_plate()));
+      for (auto fixture : p.fixtures()) {
+	REQUIRE(!(fixture.v.has_protective_base_plate()));
       }
+
+      // vector<fixture> all_orients =
+      // 	all_stable_fixtures(outer_surfs, fixes);
+
+      // surface_map orients =
+      // 	pick_orientations(mesh, surfs_to_cut, all_orients);
+
+      // REQUIRE(orients.size() == 1);
+
+      // // No use of base plates
+      // for (auto fixture : orients) {
+      // 	REQUIRE(!(all_orients[fixture.first].v.has_protective_base_plate()));
+      // }
     }
 
-    SECTION("each orientation has 1 connected component") {
-      vector<fixture> all_orients =
-	all_stable_fixtures(outer_surfs, fixes);
+    // SECTION("each orientation has 1 connected component") {
+    //   vector<fixture> all_orients =
+    // 	all_stable_fixtures(outer_surfs, fixes);
 
-      surface_map orients =
-	pick_orientations(mesh, surfs_to_cut, all_orients);
+    //   surface_map orients =
+    // 	pick_orientations(mesh, surfs_to_cut, all_orients);
       
-      for (auto p : orients) {
-	auto surface_inds = p.second;
-	auto connected_comps =
-	  connected_components_by(surface_inds,
-				  [surfs_to_cut](const unsigned i, const unsigned j)
-				  {
-				    return surfaces_share_edge(i, j, surfs_to_cut);
-				  });
-	REQUIRE(connected_comps.size() == 1);
-      }
-    }
+    //   for (auto p : orients) {
+    // 	auto surface_inds = p.second;
+    // 	auto connected_comps =
+    // 	  connected_components_by(surface_inds,
+    // 				  [surfs_to_cut](const unsigned i, const unsigned j)
+    // 				  {
+    // 				    return surfaces_share_edge(i, j, surfs_to_cut);
+    // 				  });
+    // 	REQUIRE(connected_comps.size() == 1);
+    //   }
+    // }
   }
   
   TEST_CASE("Tapered extrude top") {
