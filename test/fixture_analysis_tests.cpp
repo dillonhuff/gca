7a6198ff68edc824c4229043cb631604a5bf74f8
Dillon Huff <dillonhuff@gmail.com>
2016-07-05 16:08:30 -0700

    fixture_analysis_tests now primarily test the number of stages in each plan using make_fixture_plan

--- test/fixture_analysis_tests.cpp
+++ test/fixture_analysis_tests.cpp
@@ -143,53 +143,21 @@ namespace gca {
     set_system_allocator(&a);
     vice test_vice = emco_vice(point(-0.8, -4.4, -3.3));
     fixtures fixes(test_vice);
+    tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
+    tool t2(0.5, 3.0, 4, HSS, FLAT_NOSE);
+    vector<tool> tools{t1, t2};
     workpiece workpiece_dims(3.5, 2.5, 2.3, ALUMINUM);
+
     auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/ComplexRectanglePart1.stl", 0.001);
     
-    SECTION("4 setups, no duplicates, each orientation has 1 connected component") {
-      auto outer_surfs = outer_surfaces(mesh);
-      auto aligned_workpiece = align_workpiece(outer_surfs, workpiece_dims);
-      classify_part_surfaces(outer_surfs, aligned_workpiece);
+    SECTION("10 setups") {
+      fixture_plan p = make_fixture_plan(mesh, fixes, tools, workpiece_dims);
 
-      auto surfs_to_cut = surfaces_to_cut(mesh, outer_surfs);
-      vector<fixture> all_orients =
-	all_stable_fixtures(outer_surfs, fixes);
-      
-      surface_map orients =
-	pick_orientations(mesh, surfs_to_cut, all_orients);
-
-      // TEST: 4 setups
-      REQUIRE(orients.size() == 4);
+      REQUIRE(p.fixtures().size() == 10);
 
       // No use of base plates
-      for (auto fixture : orients) {
-	REQUIRE(!(all_orients[fixture.first].v.has_protective_base_plate()));
-      }
-
-
-      // NO DUPLICATION
-      for (unsigned i = 0; i < orients.size(); i++) {
-	for (unsigned j = i + 1; j < orients.size(); j++) {
-	  if (orients.find(i) != end(orients) &&
-	      orients.find(j) != end(orients) &&
-	      i != j) {
-	    vector<unsigned> ig = orients[i];
-	    vector<unsigned> jg = orients[j];
-	    REQUIRE(intersection(ig, jg).size() == 0);
-	  }
-	}
-      }
-      
-      // TEST: Each has 1 connected component
-      for (auto p : orients) {
-	auto surface_inds = p.second;
-	auto connected_comps =
-	  connected_components_by(surface_inds,
-				  [surfs_to_cut](const unsigned i, const unsigned j)
-				  {
-				    return surfaces_share_edge(i, j, surfs_to_cut);
-				  });
-	REQUIRE(connected_comps.size() == 1);
+      for (auto fixture : p.fixtures()) {
+	REQUIRE(!(fixture.v.has_protective_base_plate()));
       }
     }
   }
