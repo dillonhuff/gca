0bb3205317187ff85a17c7f150103b1447c5b7fd
Dillon Huff <dillonhuff@gmail.com>
2016-08-02 13:55:56 -0700

    Getting closer to correct multiple extrusion

--- src/geometry/extrusion.cpp
+++ src/geometry/extrusion.cpp
@@ -43,6 +43,16 @@ namespace gca {
     // TODO: Proper normal computation
     return oriented_polygon(dir, poly_points);
   }
+
+  std::vector<gca::edge>
+  index_poly_edges(const index_poly& p) {
+    std::vector<gca::edge> edges;
+    for (index_t i = 0; i < p.size(); i++) {
+      index_t j = (i + 1) % p.size();
+      edges.push_back(edge(p[i], p[j]));
+    }
+    return edges;
+  }
   
   std::vector<oriented_polygon>
   side_polys(const std::vector<point>& pts,
@@ -50,11 +60,7 @@ namespace gca {
 	     const double d0,
 	     const double d1,
 	     const point e) {
-    std::vector<gca::edge> edges;
-    for (index_t i = 0; i < p.size(); i++) {
-      index_t j = (i + 1) % p.size();
-      edges.push_back(edge(p[i], p[j]));
-    }
+    auto edges = index_poly_edges(p);
     std::vector<oriented_polygon> side_rects;
     for (auto ed : edges) {
       side_rects.push_back(side_rect(pts, ed, d0, d1, e));
@@ -62,6 +68,64 @@ namespace gca {
     return side_rects;
   }
 
+  template<typename T>
+  std::vector<T>
+  symmetric_difference(const std::vector<T>& a,
+		       const std::vector<T>& b) {
+    auto ac = a;
+    subtract(ac, b);
+
+    auto bc = b;
+    subtract(bc, a);
+
+    concat(ac, bc);
+
+    return ac;
+  }
+
+  index_poly collect_polygon(vector<gca::edge>& lines) {
+    assert(lines.size() > 0);
+    vector<index_t> points;
+    vector<gca::edge> to_remove;
+    points.push_back(lines.front().l);
+    points.push_back(lines.front().r);
+    lines.erase(lines.begin());
+    unsigned i = 0;
+    while (lines.size() > 0 && i < lines.size()) {
+      if (lines[i].l == points.back()) { //within_eps(lines[i].start, points.back())) {
+    	if (lines[i].r == points.front()) {//within_eps(lines[i].end, points.front())) {
+    	  lines.erase(lines.begin() + i);	  
+    	  return points;
+    	}
+    	points.push_back(lines[i].r); //lines[i].end);
+    	lines.erase(lines.begin() + i);
+    	i = 0;
+      } else if (lines[i].r == points.back()) {//within_eps(lines[i].end, points.back())) {
+    	if (lines[i].l == points.front()) {//within_eps(lines[i].start, points.front())) {
+    	  lines.erase(lines.begin() + i);
+    	  return points;
+    	}
+    	points.push_back(lines[i].l); //lines[i].start);
+    	lines.erase(lines.begin() + i);
+    	i = 0;
+      } else {
+    	i++;
+      }
+    }
+    return points;
+  }
+
+  std::vector<index_poly>
+  unordered_segments_to_index_polygons(std::vector<gca::edge>& lines) {
+    vector<index_poly> ps;
+    auto ls = lines;
+    while (ls.size() > 0) {
+      index_poly vertices = collect_polygon(ls);
+      ps.push_back(vertices);
+    }
+    return ps;
+  }
+  
   triangular_mesh
   extrude_layers(const std::vector<point>& pts,
 		 const std::vector<index_poly>& poly_layers,
@@ -72,11 +136,16 @@ namespace gca {
     std::vector<oriented_polygon> polys;
 
     double last_depth_offset = 0.0;
+    std::vector<gca::edge> last_edges;
     for (unsigned i = 0; i < layer_depths.size(); i++) {
+      auto current_edges = index_poly_edges(poly_layers[i]);
       // Front polys
-      if (i == 0) {
-	polys.push_back(convert_index_poly(pts, poly_layers[i], last_depth_offset, extrude_dir));
-      }
+      auto front_edges = symmetric_difference(current_edges, last_edges);
+      auto front_polys =
+	unordered_segments_to_index_polygons(front_edges);
+
+      polys.push_back(convert_index_poly(pts, poly_layers[i], last_depth_offset, extrude_dir));
+
 
       // Sides
       concat(polys, side_polys(pts, poly_layers[i], last_depth_offset, layer_depths[i], extrude_dir));
@@ -91,6 +160,7 @@ namespace gca {
 	polys.push_back(back_rev);
       }
 
+      last_edges = current_edges;
       last_depth_offset += layer_depths[i];
     }
     return mesh_for_polys(polys);
