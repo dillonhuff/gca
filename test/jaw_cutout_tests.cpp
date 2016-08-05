fbe257c6e95c449a4de99c3c4fd43bce25805dbe
Dillon Huff <dillonhuff@gmail.com>
2016-08-04 21:43:52 -0700

    Added basic test of jaw cutouts from clipped cylinder

--- test/jaw_cutout_tests.cpp
+++ test/jaw_cutout_tests.cpp
@@ -1,6 +1,7 @@
 #include "catch.hpp"
 
 #include "synthesis/jaw_cutout.h"
+#include "system/parse_stl.h"
 
 namespace gca {
 
@@ -16,13 +17,15 @@ namespace gca {
     vector<tool> tools{t1};
     workpiece workpiece_dims(3.0, 1.9, 3.0, ACETAL);
 
-    auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/ClippedCylinder.stl", 0.001);
+    auto part_mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/ClippedCylinder.stl", 0.001);
 
-    auto decomp = contour_surface_decomposition_in(part_mesh, point(0, 0, 1));
+    auto decomp = contour_surface_decomposition_in_dir(part_mesh, point(0, 0, 1));
     REQUIRE(decomp);
     
-    soft_jaws jaws = make_soft_jaws(*decomp, v);
+    soft_jaws jaws = make_soft_jaws(*decomp, test_vice, -1*(decomp->n));
+    triangular_mesh* axis_jaw = jaws.a_jaw;
 
-    REQUIRE();
+    vector<surface> surfs = surfaces_to_cut(*axis_jaw);
+    REQUIRE(surfs.size() == 8);
   }
 }
