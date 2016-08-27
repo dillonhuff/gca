12875994320902e20e3a45dfe5f0213c19c0396f
Dillon Huff <dillonhuff@gmail.com>
2016-08-27 16:03:22 -0700

    Fixed sign error in rotation. The bug was very hard to see. After quickly trying a few haphazard fixes I went back and carefully broke up the code until the error was obvious

--- src/geometry/rotation.cpp
+++ src/geometry/rotation.cpp
@@ -5,8 +5,93 @@
 
 namespace gca {
 
-  rotation rotate_from_to(const point from, const point to) {
-    double theta = angle_between(from, to);
+  void test_rotation(const point from_unit, const point to_unit, const rotation& r) {
+    cout << "r * from_unit = " << times_3(r, from_unit) << endl;
+
+    double d = determinant(r);
+    if (!(within_eps(d, 1.0, 0.001))) {
+      cout << "ERROR: determinant of rotation = " << d << endl;
+      cout << "from unit normal = " << from_unit << endl;
+      cout << "to unit normal = " << to_unit << endl;
+
+      double theta = angle_between(from_unit, to_unit);
+      cout << "theta = " << theta << endl;
+      cout << r << endl;
+
+      point rfu = times_3(r, from_unit);
+      double res_angle = angle_between(rfu, to_unit);
+
+      cout << "r * from = " << rfu << endl;
+      cout << "resulting angle = " << res_angle << endl;
+
+      // cout << "c = " << c << endl;
+      // cout << "s = " << s << endl;
+      // cout << "v = " << v << endl;
+
+      // cout << "vx = " << endl;
+      // cout << vx << endl;
+      
+      DBG_ASSERT(false);
+    }
+
+    if (!(within_eps(angle_between(times_3(r, from_unit), to_unit), 0.0, 0.1))) {
+      cout << "ERROR: Incorrect rotation " << endl;
+      cout << r << endl;
+
+      cout << "from unit normal = " << from_unit << endl;
+      cout << "to unit normal = " << to_unit << endl;
+      
+      cout << "r*" << from_unit << " = " << times_3(r, from_unit) << " != " << to_unit << endl;
+
+      DBG_ASSERT(false);
+    }
+    
+  }  
+
+  rotation rotate_unit_from_to_regular(const point from_unit, const point to_unit) {
+    point v = cross(from_unit, to_unit);
+    double s = v.len();
+    double c = dot(from_unit, to_unit);
+
+    cout << "c = " << c << endl;
+    cout << "s = " << s << endl;
+    cout << "v = " << v << endl;
+    
+    boost::numeric::ublas::matrix<double> vx(3, 3);
+    vx(0, 0) = 0;
+    vx(0, 1) = -v.z;
+    vx(0, 2) = v.y;
+
+    vx(1, 0) = v.z;
+    vx(1, 1) = 0;
+    vx(1, 2) = -v.x;
+
+    vx(2, 0) = -v.y;
+    vx(2, 1) = v.x;
+    vx(2, 2) = 0;
+
+    cout << "VX = " << endl;
+    cout << vx << endl;
+
+    const boost::numeric::ublas::matrix<double> id =
+      boost::numeric::ublas::identity_matrix<double>(3);
+
+    const boost::numeric::ublas::matrix<double> vxc = vx;
+    
+    const boost::numeric::ublas::matrix<double> vx2 = prod(vx, vx);
+
+    const ublas::matrix<double> r =
+      id + vxc + ((1.0 - c)/(s*s))*vx2;
+
+    return r;
+  }
+
+  rotation rotate_unit_from_to(const point from_unit, const point to_unit) {
+
+    DBG_ASSERT(within_eps(from_unit.len(), 1.0, 0.0000001));
+    DBG_ASSERT(within_eps(to_unit.len(),   1.0, 0.0000001));
+    
+    double theta = angle_between(from_unit, to_unit);
 
     std::cout << "theta = " << theta << endl;
     
@@ -17,86 +102,21 @@ namespace gca {
     if (within_eps(theta, 180, 0.01)) {
       return -1*boost::numeric::ublas::identity_matrix<double>(3);
     }
+
+    const ublas::matrix<double> r =
+      rotate_unit_from_to_regular(from_unit, to_unit);
+
+    test_rotation(from_unit, to_unit, r);
+
+    return r;
     
+  }
+
+  rotation rotate_from_to(const point from, const point to) {
     point from_unit = from.normalize();
     point to_unit = to.normalize();
 
-    // point v = cross(from_unit, to_unit).normalize();
-    // //    double s = v.len();
-    // double dt = from_unit.dot(to_unit);
-
-    // double th = safe_acos(dt / (from_unit.len() * to_unit.len()));
-
-    point v = cross(from, to).normalize();
-    theta = safe_acos(from.dot(to) / (from.len() * to.len()));
-
-    boost::numeric::ublas::matrix<double> vx(3, 3);
-    vx(0, 0) = 0;
-    vx(0, 1) = -v.z;
-    vx(0, 2) = -v.y;
-
-    vx(1, 0) = v.z;
-    vx(1, 1) = 0;
-    vx(1, 2) = -v.x;
-
-    vx(2, 0) = -v.y;
-    vx(2, 1) = v.x;
-    vx(2, 2) = 0;
-
-    auto id = boost::numeric::ublas::identity_matrix<double>(3);
-
-    const boost::numeric::ublas::matrix<double> vxc = vx;
-    
-    auto vx2 = prod(vx, vx);
-    const ublas::matrix<double> r =
-      id + sin(theta) * vxc + (1.0 - cos(theta))*vx2; //id + vxc + ((1.0 - c)/(s*s))*vx2;
-
-    cout << "r * from_unit = " << times_3(r, from_unit) << endl;
-
-    double d = determinant(r);
-    if (!(within_eps(d, 1.0, 0.001))) {
-      cout << "ERROR: determinant of rotation = " << d << endl;
-      cout << "from = " << from << endl;
-      cout << "to = " << to << endl;
-      cout << "from unit normal = " << from_unit << endl;
-      cout << "to unit normal = " << to_unit << endl;
-
-      cout << "theta = " << theta << endl;
-      cout << r << endl;
-
-      point rfu = times_3(r, from_unit);
-      double res_angle = angle_between(rfu, to_unit);
-
-      cout << "r * from_unit = " << rfu << endl;
-      cout << "resulting angle = " << res_angle << endl;
-
-      // cout << "c = " << c << endl;
-      // cout << "s = " << s << endl;
-      // cout << "v = " << v << endl;
-
-      cout << "vx = " << endl;
-      cout << vx << endl;
-      
-      DBG_ASSERT(false);
-    }
-
-    if (!(within_eps(angle_between(times_3(r, from_unit), to_unit), 0.0, 0.1))) {
-      cout << "ERROR: Incorrect rotation " << endl;
-      cout << r << endl;
-
-      cout << "from = " << from << endl;
-      cout << "to = " << to << endl;
-      cout << "from unit normal = " << from_unit << endl;
-      cout << "to unit normal = " << to_unit << endl;
-      
-      cout << "r*" << from_unit << " = " << times_3(r, from_unit) << " != " << to_unit << endl;
-      // cout << "c = " << c << endl;
-      // cout << "s = " << s << endl;
-      // cout << "v = " << v << endl;
-      DBG_ASSERT(false);
-    }
-    
-    return r;
+    return rotate_unit_from_to(from_unit, to_unit);
   }
 
   triangular_mesh apply(const rotation& r, const triangular_mesh& m) {
--- test/rotation_tests.cpp
+++ test/rotation_tests.cpp
@@ -3,6 +3,24 @@
 
 namespace gca {
 
+  TEST_CASE("Simple 90 degree rotation") {
+    point from(0, 1, 0);
+    point to(0, 0, 1);
+
+    const rotation r = rotate_from_to(from, to);
+
+    REQUIRE(within_eps(determinant(r), 1.0, 0.001));
+  }
+
+  TEST_CASE("Simple 2 component 90 degree rotation") {
+    point from(1, 1, 0);
+    point to(0, 0, 1);
+
+    const rotation r = rotate_from_to(from, to);
+
+    REQUIRE(within_eps(determinant(r), 1.0, 0.001));
+  }
+  
   TEST_CASE("All positive rotation, not 90 degrees") {
     point from(7.59, 3.4, 9.32);
     point to(0, 0, 1);
