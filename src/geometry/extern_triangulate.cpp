3b498494862c7ecfea65338944d600afcad5dc0e
Dillon Huff <dillonhuff@gmail.com>
2016-09-21 15:00:05 -0700

    Creating helper functions for triangulation

--- src/geometry/extern_triangulate.cpp
+++ src/geometry/extern_triangulate.cpp
@@ -163,34 +163,56 @@ namespace gca {
     return tris;
   }
 
+  void set_points(struct triangulateio* in, const polygon_3& p) {
+    in->numberofpoints = 4;
+    in->numberofpointattributes = 0; //1;
+    in->pointlist = (REAL *) malloc(in->numberofpoints * 2 * sizeof(REAL));
+    in->pointlist[0] = 0.0;
+    in->pointlist[1] = 0.0;
+    in->pointlist[2] = 1.0;
+    in->pointlist[3] = 0.0;
+    in->pointlist[4] = 1.0;
+    in->pointlist[5] = 10.0;
+    in->pointlist[6] = 0.0;
+    in->pointlist[7] = 10.0;
+
+    in->pointattributelist = NULL;
+
+    in->pointmarkerlist = (int *) malloc(in->numberofpoints * sizeof(int));
+    in->pointmarkerlist[0] = 0;
+    in->pointmarkerlist[1] = 2;
+    in->pointmarkerlist[2] = 0;
+    in->pointmarkerlist[3] = 0;
+  }
+
+  void set_output(struct triangulateio* mid) {
+    /* Make necessary initializations so that Triangle can return a */
+    /*   triangulation in `mid' */
+
+    mid->pointlist = (REAL *) NULL;            /* Not needed if -N switch used. */
+    /* Not needed if -N switch used or number of point attributes is zero: */
+    mid->pointattributelist = (REAL *) NULL;
+    mid->pointmarkerlist = (int *) NULL; /* Not needed if -N or -B switch used. */
+    mid->trianglelist = (int *) NULL;          /* Not needed if -E switch used. */
+    /* Not needed if -E switch used or number of triangle attributes is zero: */
+    mid->triangleattributelist = (REAL *) NULL;
+    mid->neighborlist = (int *) NULL;         /* Needed only if -n switch used. */
+    /* Needed only if segments are output (-p or -c) and -P not used: */
+    mid->segmentlist = (int *) NULL;
+    /* Needed only if segments are output (-p or -c) and -P and -B not used: */
+    mid->segmentmarkerlist = (int *) NULL;
+    mid->edgelist = (int *) NULL;             /* Needed only if -e switch used. */
+    mid->edgemarkerlist = (int *) NULL;   /* Needed if -e used and -B not used. */
+
+    /* Triangulate the points.  Switches are chosen to read and write a  */
+  }
+
   std::vector<triangle> triangulate_flat_3d(const polygon_3& p) {
     struct triangulateio in, mid;
 
     /* Define input points. */
 
-    in.numberofpoints = 4;
-    in.numberofpointattributes = 1;
-    in.pointlist = (REAL *) malloc(in.numberofpoints * 2 * sizeof(REAL));
-    in.pointlist[0] = 0.0;
-    in.pointlist[1] = 0.0;
-    in.pointlist[2] = 1.0;
-    in.pointlist[3] = 0.0;
-    in.pointlist[4] = 1.0;
-    in.pointlist[5] = 10.0;
-    in.pointlist[6] = 0.0;
-    in.pointlist[7] = 10.0;
-    in.pointattributelist = (REAL *) malloc(in.numberofpoints *
-					    in.numberofpointattributes *
-					    sizeof(REAL));
-    in.pointattributelist[0] = 0.0;
-    in.pointattributelist[1] = 1.0;
-    in.pointattributelist[2] = 11.0;
-    in.pointattributelist[3] = 10.0;
-    in.pointmarkerlist = (int *) malloc(in.numberofpoints * sizeof(int));
-    in.pointmarkerlist[0] = 0;
-    in.pointmarkerlist[1] = 2;
-    in.pointmarkerlist[2] = 0;
-    in.pointmarkerlist[3] = 0;
+    set_points(&in, p);
 
     in.numberofsegments = 0;
     in.numberofholes = 0;
@@ -204,25 +226,9 @@ namespace gca {
     printf("Input point set:\n\n");
     report(&in, 1, 0, 0, 0, 0, 0);
 
-    /* Make necessary initializations so that Triangle can return a */
-    /*   triangulation in `mid' */
+    set_output(&mid);
 
-    mid.pointlist = (REAL *) NULL;            /* Not needed if -N switch used. */
-    /* Not needed if -N switch used or number of point attributes is zero: */
-    mid.pointattributelist = (REAL *) NULL;
-    mid.pointmarkerlist = (int *) NULL; /* Not needed if -N or -B switch used. */
-    mid.trianglelist = (int *) NULL;          /* Not needed if -E switch used. */
-    /* Not needed if -E switch used or number of triangle attributes is zero: */
-    mid.triangleattributelist = (REAL *) NULL;
-    mid.neighborlist = (int *) NULL;         /* Needed only if -n switch used. */
-    /* Needed only if segments are output (-p or -c) and -P not used: */
-    mid.segmentlist = (int *) NULL;
-    /* Needed only if segments are output (-p or -c) and -P and -B not used: */
-    mid.segmentmarkerlist = (int *) NULL;
-    mid.edgelist = (int *) NULL;             /* Needed only if -e switch used. */
-    mid.edgemarkerlist = (int *) NULL;   /* Needed if -e used and -B not used. */
 
-    /* Triangulate the points.  Switches are chosen to read and write a  */
     /*   PSLG (p), preserve the convex hull (c), number everything from  */
     /*   zero (z), assign a regional attribute to each element (A), and  */
     /*   produce an edge list (e), a Voronoi diagram (v), and a triangle */
