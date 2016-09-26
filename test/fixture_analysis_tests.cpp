c31f54b198d0769605cd6e0fb7d7793a72b5b72f
Dillon Huff <dillonhuff@gmail.com>
2016-09-25 19:26:44 -0700

    Removed some obsolete and redundant test code

--- test/fixture_analysis_tests.cpp
+++ test/fixture_analysis_tests.cpp
@@ -5,51 +5,6 @@
 
 namespace gca {
 
-  // NOTE: This duplicate of a test in mesh_to_gcode is too expensive to keep around
-  // TEST_CASE("Parallel plates") {
-  //   arena_allocator a;
-  //   set_system_allocator(&a);
-
-  //   // Change back to emco_vice
-  //   vice test_vice = large_jaw_vice(5, point(-0.8, -4.4, -3.3));
-  //   std::vector<plate_height> parallel_plates{0.5, 0.7};
-  //   fixtures fixes(test_vice, parallel_plates);
-
-  //   tool t1(0.1, 3.0, 4, HSS, FLAT_NOSE);
-  //   t1.set_cut_diameter(0.1);
-  //   t1.set_cut_length(0.4);
-
-  //   t1.set_shank_diameter(3.0 / 8.0);
-  //   t1.set_shank_length(0.1);
-
-  //   t1.set_holder_diameter(2.0);
-  //   t1.set_holder_length(2.5);
-
-  //   tool t2(0.12, 3.0, 4, HSS, FLAT_NOSE);
-  //   t2.set_cut_diameter(0.12);
-  //   t2.set_cut_length(1.2);
-
-  //   t2.set_shank_diameter(0.1);
-  //   t2.set_shank_length(0.5);
-
-  //   t2.set_holder_diameter(2.0);
-  //   t2.set_holder_length(2.5);
-    
-  //   vector<tool> tools{t1, t2};
-
-  //   workpiece workpiece_dims(3.0, 1.9, 3.0, ACETAL);
-    
-  //   SECTION("Clipped pill") {
-
-
-  //     auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/ClippedPill.stl", 0.001);
-
-  //     fixture_plan p = make_fixture_plan(mesh, fixes, tools, {workpiece_dims});
-
-  //     REQUIRE(p.fixtures().size() == 2);
-  //   }
-  // }
-
   TEST_CASE("More parallel plates") {
     arena_allocator a;
     set_system_allocator(&a);
