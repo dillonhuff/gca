156732c19561604946fd974aae1d83c3955a5c1a
Dillon Huff <dillonhuff@gmail.com>
2016-09-25 18:36:31 -0700

    Now have slightly better nef_based_planning results. Still need to handle volume tracking better

--- src/process_planning/job_planning.cpp
+++ src/process_planning/job_planning.cpp
@@ -61,6 +61,45 @@ namespace gca {
     return subtracted;
   }
 
+  double volume(const feature& f) {
+    const rotation r = rotate_from_to(f.normal(), point(0, 0, 1));
+    auto bp = to_boost_poly_2(apply(r, f.base()));
+
+    double base_area = bg::area(bp);
+    
+    return base_area * f.depth();
+  }
+
+  double volume(feature_decomposition* f) {
+    auto feats = collect_features(f);
+
+    double vol = 0.0;
+    for (auto ft : feats) {
+      vol += volume(*ft);
+    }
+
+    return vol;
+  }
+
+  direction_process_info
+  select_next_dir(std::vector<direction_process_info>& dir_info) {
+    DBG_ASSERT(dir_info.size() > 0);
+
+    auto next = max_element(begin(dir_info), end(dir_info),
+			    [](const direction_process_info& l,
+			       const direction_process_info& r) {
+			      return volume(l.decomp) < volume(r.decomp);
+			    });
+
+    DBG_ASSERT(next != end(dir_info));
+
+    direction_process_info next_elem = *next;
+
+    dir_info.erase(next);
+
+    return next_elem;
+  }
+
   std::vector<fixture_setup> plan_jobs(const triangular_mesh& stock,
 				       const triangular_mesh& part,
 				       const fixtures& f,
@@ -90,9 +129,17 @@ namespace gca {
     triangular_mesh current_stock = stock;
     vector<fixture_setup> cut_setups;
 
-    //vtk_debug_mesh(current_stock);
+    for (auto info : dir_info) {
+      feature_decomposition* decomp = info.decomp;
+      tool_access_info& acc_info = info.tool_info;
+      delete_nodes(decomp, [acc_info](feature* f) {
+	  return map_find(f, acc_info).size() == 0;
+	});
+    }
 
-    for (auto& info : dir_info) {
+    while (dir_info.size() > 0) {
+      direction_process_info info = select_next_dir(dir_info);
+
       auto sfs = outer_surfaces(current_stock);
       auto orients = all_stable_orientations(sfs, v);
       point n = normal(info.decomp);
@@ -106,10 +153,6 @@ namespace gca {
 
 	auto decomp = info.decomp;
 	auto& acc_info = info.tool_info;
-
-	delete_nodes(decomp, [acc_info](feature* f) {
-	    return map_find(f, acc_info).size() == 0;
-	  });
 
 	auto t = mating_transform(current_stock, orient, v);
 	cut_setups.push_back(create_setup(t, current_stock, part, decomp, fix, info.tool_info));
