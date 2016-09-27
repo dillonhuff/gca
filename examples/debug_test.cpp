3c4f6935650072287333babf705669fa64c870bc
Dillon Huff <dillonhuff@gmail.com>
2016-09-27 11:20:16 -0700

    Several of the failures were just due to lack of appropriate tools

--- examples/debug_test.cpp
+++ examples/debug_test.cpp
@@ -1,51 +1,82 @@
 #define CATCH_CONFIG_MAIN
 
+#include "analysis/gcode_to_cuts.h"
 #include "catch.hpp"
+#include "geometry/triangular_mesh.h"
 #include "synthesis/fixture_analysis.h"
+#include "synthesis/mesh_to_gcode.h"
+#include "synthesis/toolpath_generation.h"
+#include "synthesis/workpiece_clipping.h"
 #include "utils/arena_allocator.h"
 #include "system/parse_stl.h"
 
 namespace gca {
 
-  TEST_CASE("More parallel plates") {
+  void sanity_check_toolpaths(const fabrication_plan& plan) {
+    for (auto step : plan.steps()) {
+      REQUIRE(step.toolpaths().size() > 0);
+    }
+  }
+
+  TEST_CASE("Box with hole") {
     arena_allocator a;
     set_system_allocator(&a);
 
-    // Change back to emco_vice
+    // TODO: Make emco_vice again
     vice test_vice = large_jaw_vice(5, point(-0.8, -4.4, -3.3));
-    std::vector<plate_height> parallel_plates{0.5, 0.7};
+    std::vector<plate_height> parallel_plates{0.1, 0.3};
     fixtures fixes(test_vice, parallel_plates);
 
-    tool t1(0.1, 3.0, 4, HSS, FLAT_NOSE);
-    t1.set_cut_diameter(0.1);
-    t1.set_cut_length(0.4);
+    tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
+    t1.set_cut_diameter(0.25);
+    t1.set_cut_length(0.6);
 
     t1.set_shank_diameter(3.0 / 8.0);
-    t1.set_shank_length(0.1);
+    t1.set_shank_length(0.3);
 
-    t1.set_holder_diameter(2.0);
-    t1.set_holder_length(2.5);
+    t1.set_holder_diameter(2.5);
+    t1.set_holder_length(3.5);
     
-    vector<tool> tools{t1};
+    tool t2(0.5, 3.0, 4, HSS, FLAT_NOSE);
+    t2.set_cut_diameter(0.5);
+    t2.set_cut_length(1.3);
 
-    workpiece workpiece_dims(3.0, 1.9, 3.0, ACETAL);
+    t2.set_shank_diameter(0.5);
+    t2.set_shank_length(0.5);
 
-    //    NOTE: This branch cannot yet handle volume tracking situations
-    //    like this one
-    SECTION("Block with hole and side pocket") {
-      auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BlockWithHoleAndSidePocket.stl", 0.0001);
+    t2.set_holder_diameter(2.5);
+    t2.set_holder_length(3.5);
 
-      fixture_plan p = make_fixture_plan(mesh, fixes, tools, {workpiece_dims});
+    vector<tool> tools{t1, t2};
+    workpiece workpiece_dims(3.0, 3.0, 3.0, ACETAL); //1.5, 1.2, 1.5, ACETAL);
 
-      REQUIRE(p.fixtures().size() == 3);
+    SECTION("Box with hole has 2 clippings") {
+      auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BoxWithTopHole.stl", 0.001);
+      auto result_programs = mesh_to_gcode(mesh, fixes, tools, workpiece_dims);
+      REQUIRE(result_programs.size() == 2);
+    }
 
-      for (auto f : p.fixtures()) {
-    	cout << "orientation = " << f.fix.orient.top_normal() << endl;
-      }
+    SECTION("Box with two holes has a contour and 1 pocketing") {
+      auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BoxWith2Holes.stl", 0.001);
+      auto result_programs = mesh_to_gcode(mesh, fixes, tools, workpiece_dims);
+      REQUIRE(result_programs.size() == 3);
+    }
 
-      REQUIRE(p.fixtures()[0].pockets.size() == 3);
-      REQUIRE(p.fixtures()[1].pockets.size() == 1);
-      REQUIRE(p.fixtures()[2].pockets.size() == 1);
+    // TODO: When part axis selection can break ties, add this part back. For now
+    // it seems like the surface area heuristic is good enough
+    // SECTION("Box with protrusion") {
+    //   // TODO: Restore old dims
+    //   workpiece workpiece_dims(3.0, 3.0, 3.0, ALUMINUM); //1.5, 1.2, 2.0, ALUMINUM);
+    //   auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BoxWithProtrusion.stl", 0.001);
+    //   auto result_programs = mesh_to_gcode(mesh, fixes, tools, workpiece_dims);
+    //   REQUIRE(result_programs.size() == 2);
+    // }
+
+    SECTION("Box with thru hole") {
+      workpiece workpiece_dims(1.51, 1.51, 2.0, ACETAL);
+      auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BoxWithThruHole.stl", 0.001);
+      auto result_programs = mesh_to_gcode(mesh, fixes, tools, workpiece_dims);
+      REQUIRE(result_programs.size() == 2);
     }
     
   }
--- test/mesh_to_gcode_tests.cpp
+++ test/mesh_to_gcode_tests.cpp
@@ -60,6 +60,39 @@ namespace gca {
 	sanity_check_toolpaths(result_plan);
       }
     }
+  }
+
+  TEST_CASE("Box with hole") {
+    arena_allocator a;
+    set_system_allocator(&a);
+
+    // TODO: Make emco_vice again
+    vice test_vice = large_jaw_vice(5, point(-0.8, -4.4, -3.3));
+    std::vector<plate_height> parallel_plates{0.1, 0.3};
+    fixtures fixes(test_vice, parallel_plates);
+
+    tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
+    t1.set_cut_diameter(0.25);
+    t1.set_cut_length(0.6);
+
+    t1.set_shank_diameter(3.0 / 8.0);
+    t1.set_shank_length(0.3);
+
+    t1.set_holder_diameter(2.5);
+    t1.set_holder_length(3.5);
+    
+    tool t2(0.5, 3.0, 4, HSS, FLAT_NOSE);
+    t2.set_cut_diameter(0.5);
+    t2.set_cut_length(0.3);
+
+    t2.set_shank_diameter(0.5);
+    t2.set_shank_length(0.5);
+
+    t2.set_holder_diameter(2.5);
+    t2.set_holder_length(3.5);
+
+    vector<tool> tools{t1, t2};
+    workpiece workpiece_dims(3.0, 3.0, 3.0, ACETAL); //1.5, 1.2, 1.5, ACETAL);
 
     SECTION("Box with hole has 2 clippings") {
       auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BoxWithTopHole.stl", 0.001);
