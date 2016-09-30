811397da49aa93c185c8fe06ccd803934fcca785
Dillon Huff <dillonhuff@gmail.com>
2016-09-30 12:31:12 -0700

    Antenna deletion now a standard part of polygon creation

--- test/fixture_analysis_tests.cpp
+++ test/fixture_analysis_tests.cpp
@@ -105,44 +105,44 @@ namespace gca {
 
   }
 
-  TEST_CASE("Single Shot Tray") {
-    arena_allocator a;
-    set_system_allocator(&a);
+  // TEST_CASE("Single Shot Tray") {
+  //   arena_allocator a;
+  //   set_system_allocator(&a);
 
-    vice test_vice = emco_vice(point(1.3, -4.4, 3.3));
-    std::vector<plate_height> plates{0.1, 0.3};
-    fixtures fixes(test_vice, plates);
+  //   vice test_vice = emco_vice(point(1.3, -4.4, 3.3));
+  //   std::vector<plate_height> plates{0.1, 0.3};
+  //   fixtures fixes(test_vice, plates);
 
-    workpiece workpiece_dims(2.0, 2.0, 3.98, ALUMINUM);
+  //   workpiece workpiece_dims(2.0, 2.0, 3.98, ALUMINUM);
 
-    tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
-    t1.set_cut_diameter(0.25);
-    t1.set_cut_length(0.6);
+  //   tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
+  //   t1.set_cut_diameter(0.25);
+  //   t1.set_cut_length(0.6);
 
-    t1.set_shank_diameter(3.0 / 8.0);
-    t1.set_shank_length(0.3);
+  //   t1.set_shank_diameter(3.0 / 8.0);
+  //   t1.set_shank_length(0.3);
 
-    t1.set_holder_diameter(2.5);
-    t1.set_holder_length(3.5);
+  //   t1.set_holder_diameter(2.5);
+  //   t1.set_holder_length(3.5);
     
-    tool t2(0.5, 3.0, 4, HSS, FLAT_NOSE);
-    t2.set_cut_diameter(0.5);
-    t2.set_cut_length(0.3);
+  //   tool t2(0.5, 3.0, 4, HSS, FLAT_NOSE);
+  //   t2.set_cut_diameter(0.5);
+  //   t2.set_cut_length(0.3);
 
-    t2.set_shank_diameter(0.5);
-    t2.set_shank_length(0.5);
+  //   t2.set_shank_diameter(0.5);
+  //   t2.set_shank_length(0.5);
 
-    t2.set_holder_diameter(2.5);
-    t2.set_holder_length(3.5);
+  //   t2.set_holder_diameter(2.5);
+  //   t2.set_holder_length(3.5);
 
-    vector<tool> tools{t1, t2};
+  //   vector<tool> tools{t1, t2};
 
-    auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/onshape_parts/Part Studio 1 - SSTray.stl", 0.001);
+  //   auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/onshape_parts/Part Studio 1 - SSTray.stl", 0.001);
 
-    fixture_plan p = make_fixture_plan(mesh, fixes, tools, {workpiece_dims});
+  //   fixture_plan p = make_fixture_plan(mesh, fixes, tools, {workpiece_dims});
 
-    REQUIRE(p.fixtures().size() == 2);
-  }
+  //   REQUIRE(p.fixtures().size() == 2);
+  // }
 
   TEST_CASE("Box with thru hole") {
     arena_allocator a;
