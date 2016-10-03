848cd592518d74eb8583f6fa263aa0bbb361458d
Dillon Huff <dillonhuff@gmail.com>
2016-10-03 15:23:44 -0700

    Dilated feature is no longer used to track the volume left in a feature. This reduces tolerance errors, but now causes a failure in block with hole

--- src/process_planning/job_planning.cpp
+++ src/process_planning/job_planning.cpp
@@ -384,19 +384,20 @@ namespace gca {
 	auto features = collect_viable_features(decomp, volume_inf, fix);
 
 	if (features.size() > 0) {
-	  for (auto f : features) {
-	    auto vol_data = map_find(f, volume_inf);
-	    cout << "delta volume = " << vol_data.volume << endl;
 
-	    vtk_debug_feature(*f);
+	  // for (auto f : features) {
+	  //   auto vol_data = map_find(f, volume_inf);
+	  //   cout << "delta volume = " << vol_data.volume << endl;
 
-	    //	  vtk_debug_mesh(vol_data.dilated_mesh);
+	  //   vtk_debug_feature(*f);
 
-	    vtk_debug_meshes(nef_polyhedron_to_trimeshes(vol_data.remaining_volume));
+	  //   //	  vtk_debug_mesh(vol_data.dilated_mesh);
 
-	  }
-	  vtk_debug_features(features);
-	  concat(all_features, features);
+	  //   vtk_debug_meshes(nef_polyhedron_to_trimeshes(vol_data.remaining_volume));
+
+	  // }
+	  // vtk_debug_features(features);
+	  // concat(all_features, features);
 
 	  cut_setups.push_back(create_setup(t, current_stock, part, features, fix, info.tool_info));
 
@@ -410,7 +411,7 @@ namespace gca {
 	  cout << "Stock volume = " << stock_volume << endl;
 	  cout << "part / stock = " << volume_ratio << endl;
 
-	  vtk_debug_mesh(current_stock);
+	  //vtk_debug_mesh(current_stock);
 
 	  if (volume_ratio > 0.999) { break; }
 	}
