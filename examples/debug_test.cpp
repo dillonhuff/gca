41008fb789c7be3d08eb0aaa054a3210bafdb8d7
Dillon Huff <dillonhuff@gmail.com>
2016-10-05 10:01:37 -0700

    Starting to work on real testing of part 1 (29) and through feature selection when through features are too long

--- examples/debug_test.cpp
+++ examples/debug_test.cpp
@@ -1,68 +1,27 @@
 #define CATCH_CONFIG_MAIN
 
-#include "analysis/gcode_to_cuts.h"
+#define CATCH_CONFIG_MAIN
+
 #include "catch.hpp"
-#include "geometry/triangular_mesh.h"
+#include "feature_recognition/feature_decomposition.h"
+#include "feature_recognition/visual_debug.h"
+#include "geometry/mesh_operations.h"
+#include "geometry/vtk_debug.h"
 #include "synthesis/fixture_analysis.h"
-#include "synthesis/mesh_to_gcode.h"
-#include "synthesis/toolpath_generation.h"
-#include "synthesis/workpiece_clipping.h"
 #include "utils/arena_allocator.h"
 #include "system/parse_stl.h"
 
 namespace gca {
 
-  TEST_CASE("More parallel plates") {
+  TEST_CASE("Single Shot Tray") {
     arena_allocator a;
     set_system_allocator(&a);
 
-    // Change back to emco_vice
-    vice test_vice = large_jaw_vice(5, point(-0.8, -4.4, -3.3));
-    std::vector<plate_height> parallel_plates{0.5, 0.7};
-    fixtures fixes(test_vice, parallel_plates);
+    vice test_vice = large_jaw_vice(6, point(1.3, -4.4, 3.3));
+    std::vector<plate_height> plates{0.1, 0.3, 0.7};
+    fixtures fixes(test_vice, plates);
 
-    tool t1(0.1, 3.0, 4, HSS, FLAT_NOSE);
-    t1.set_cut_diameter(0.1);
-    t1.set_cut_length(0.4);
-
-    t1.set_shank_diameter(3.0 / 8.0);
-    t1.set_shank_length(0.1);
-
-    t1.set_holder_diameter(2.0);
-    t1.set_holder_length(2.5);
-    
-    vector<tool> tools{t1};
-
-    workpiece workpiece_dims(3.0, 1.9, 3.0, ACETAL);
-
-    SECTION("Block with hole and side pocket") {
-      auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BlockWithHoleAndSidePocket.stl", 0.0001);
-
-      fixture_plan p = make_fixture_plan(mesh, fixes, tools, {workpiece_dims});
-
-      REQUIRE(p.fixtures().size() == 3);
-
-      for (auto f : p.fixtures()) {
-    	cout << "orientation = " << f.fix.orient.top_normal() << endl;
-      }
-
-      REQUIRE(p.fixtures()[0].pockets.size() == 3);
-      REQUIRE(p.fixtures()[1].pockets.size() == 1);
-      REQUIRE(p.fixtures()[2].pockets.size() == 1);
-    }
-
-  }
-  
-  void sanity_check_toolpaths(const fabrication_plan& plan) {
-    for (auto step : plan.steps()) {
-      REQUIRE(step.toolpaths().size() > 0);
-    }
-  }
-
-
-  TEST_CASE("Part with hole unreachable from the top") {
-    arena_allocator a;
-    set_system_allocator(&a);
+    workpiece workpiece_dims(5.0, 5.0, 5.0, ALUMINUM);
 
     tool t1(0.25, 3.0, 4, HSS, FLAT_NOSE);
     t1.set_cut_diameter(0.25);
@@ -74,18 +33,18 @@ namespace gca {
     t1.set_holder_diameter(2.5);
     t1.set_holder_length(3.5);
     
-    tool t2(0.1, 3.0, 4, HSS, FLAT_NOSE);
-    t2.set_cut_diameter(0.1);
-    t2.set_cut_length(1.0);
+    tool t2(0.5, 3.0, 4, HSS, FLAT_NOSE);
+    t2.set_cut_diameter(0.5);
+    t2.set_cut_length(0.3);
 
     t2.set_shank_diameter(0.5);
-    t2.set_shank_length(0.4);
+    t2.set_shank_length(0.5);
 
     t2.set_holder_diameter(2.5);
     t2.set_holder_length(3.5);
 
     tool t3{0.2334, 3.94, 4, HSS, FLAT_NOSE};
-    t3.set_cut_diameter(0.2334);
+    t3.set_cut_diameter(0.12);
     t3.set_cut_length(1.2);
 
     t3.set_shank_diameter(0.5);
@@ -93,145 +52,38 @@ namespace gca {
 
     t3.set_holder_diameter(2.5);
     t3.set_holder_length(3.5);
+
+    // Ridiculous tool used to test feasability
+    tool t4{0.2334, 3.94, 4, HSS, FLAT_NOSE};
+    t4.set_cut_diameter(0.05);
+    t4.set_cut_length(1.2);
+
+    t4.set_shank_diameter(0.5);
+    t4.set_shank_length(0.05);
+
+    t4.set_holder_diameter(2.5);
+    t4.set_holder_length(3.5);
     
-    std::vector<tool> tools{t1, t2, t3};
+    vector<tool> tools{t1, t2, t3, t4};
 
-    vice test_vice = large_jaw_vice(5, point(-0.8, -4.4, -3.3));
-    std::vector<plate_height> parallel_plates{0.1, 0.3, 0.5};
-    fixtures fixes(test_vice, parallel_plates);
+    vector<string> part_paths{
+      "test/stl-files/onshape_parts//Part Studio 1 - Part 1(29).stl",
+	};
 
-    workpiece workpiece_dims(3.0, 3.0, 3.0, ACETAL);
+    for (auto part_path : part_paths) { //passes) { //part_paths) {
+      cout << "Part path: " << part_path << endl;
 
-    auto mesh = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/onshape_parts/Part Studio 1 - Part 1.stl", 0.0001);
+      auto mesh = parse_stl(part_path, 0.001);
 
-    auto result_plan = make_fabrication_plan(mesh, fixes, tools, {workpiece_dims});
+      box bounding = mesh.bounding_box();
 
-    // TODO: Get this down to 2 setups using better volume removal analysis
-    SECTION("3 setups") {
-      REQUIRE(result_plan.steps().size() == 3);
+      cout << "Bounding box = " << endl;
+      cout << bounding << endl;
 
-      sanity_check_toolpaths(result_plan);
+      fixture_plan p = make_fixture_plan(mesh, fixes, tools, {workpiece_dims});
+
+      cout << "Number of steps = " << p.fixtures().size() << endl;
     }
-
-  }
-
-  TEST_CASE("Outer surfaces") {
-    arena_allocator a;
-    set_system_allocator(&a);
-
-    SECTION("Simple box") {
-      auto box_triangles = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/Cube0p5.stl").triangles;
-      auto mesh = make_mesh(box_triangles, 0.001);
-      auto surfaces = outer_surfaces(mesh);
-
-      
-
-      SECTION("Simple box has 6 outer surfaces") {
-	REQUIRE(surfaces.size() == 6);
-      }
-
-      SECTION("All simple box surfaces are part of a SA faces") {
-	vector<index_t> fis = mesh.face_indexes();
-	workpiece workpiece_dims(1.5, 1.2, 1.5, ALUMINUM);
-	auto workpiece_mesh = align_workpiece(surfaces, workpiece_dims);
-	auto clipped_surfs =
-	  stable_surfaces_after_clipping(mesh, workpiece_mesh);
-	remove_contained_surfaces(clipped_surfs, surfaces);
-	
-	REQUIRE(surfaces.size() == 0);
-      }
-    }
-
-    SECTION("Box with 1 hole") {
-      auto box_triangles = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BoxWithTopHole.stl").triangles;
-      auto mesh = make_mesh(box_triangles, 0.001);
-      auto surfaces = outer_surfaces(mesh);
-      SECTION("A box with 1 hole has 6 outer surfaces") {
-	REQUIRE(surfaces.size() == 6);
-      }
-    }
-
-    SECTION("Box with 2 holes") {
-      auto box_triangles = parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/BoxWith2Holes.stl").triangles;
-      auto mesh = make_mesh(box_triangles, 0.001);
-      auto surfaces = outer_surfaces(mesh);
-
-      SECTION("A box with 2 holes has 6 outer surfaces") {
-	REQUIRE(surfaces.size() == 6);
-      }
-    }
-  }
-
-  void test_jaw_alignment(const rigid_arrangement& a,
-			  const vice& v) {
-    REQUIRE(a.mesh_names().size() == 5);
-
-    plane a_jaw_base = surface_plane(a.labeled_surface("a_jaw", "base"));
-
-    REQUIRE(within_eps(angle_between(a_jaw_base.normal(), point(0, 0, -1)),  0, 1.0));
-
-    double vice_z = v.base_z();
-
-    REQUIRE(within_eps(a_jaw_base.pt().z, vice_z, 0.001));
-
-    point z_axis(0, 0, 1);
-    double part_top = max_point_in_dir(a.mesh("part"), z_axis).z;
-    double jaw_top = max_point_in_dir(a.mesh("an_jaw"), z_axis).z;
-
-    REQUIRE(part_top > jaw_top);
-    
-  }
-
-  template<typename A, typename B>
-  void print_map_info(const std::map<A, B>& m) {
-    for (auto p : m) {
-      cout << p.first << " -> " << p.second << endl;
-    }
-  }
-  
-  void test_no_empty_toolpaths(const fabrication_plan& plan) {
-    int num_empty_toolpaths = 0;
-
-    std::map<pocket_name, int> empty_map;
-    std::map<pocket_name, int> total_map;
-    
-    for (auto s : plan.steps()) {
-      for (auto& tp : s.toolpaths()) {
-	if (total_map.find(tp.pocket_type()) != end(total_map)) {
-	  total_map[tp.pocket_type()] = total_map[tp.pocket_type()] + 1;
-	} else {
-	  total_map[tp.pocket_type()] = 1;
-	}
-
-	if (tp.lines.size() == 0) {
-
-	  if (empty_map.find(tp.pocket_type()) != end(empty_map)) {
-	    empty_map[tp.pocket_type()] = empty_map[tp.pocket_type()] + 1;
-	  } else {
-	    empty_map[tp.pocket_type()] = 1;
-	  }
-
-	  num_empty_toolpaths++;
-	}
-      }
-    }
-
-    cout << "Total toolpaths" << endl;
-    print_map_info(total_map);
-
-    cout << "Empty toolpaths" << endl;
-    print_map_info(empty_map);
-    REQUIRE(num_empty_toolpaths == 0);
-  }
-
-  void test_no_freeform_pockets(const fixture_plan& fix_plan) {
-    int num_freeform_pockets = 0;
-    for (auto f : fix_plan.fixtures()) {
-      for (auto pocket : f.pockets) {
-	if (pocket.pocket_type() == FREEFORM_POCKET) { num_freeform_pockets++; }
-      }
-    }
-    REQUIRE(num_freeform_pockets == 0);
   }
 
 }
