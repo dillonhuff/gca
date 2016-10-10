a8bad2018951e9fad6980213907c16d4d73d3c1c
Dillon Huff <dillonhuff@gmail.com>
2016-10-09 22:27:34 -0700

    Added tool.cpp to the repo

---
+++ src/synthesis/tool.cpp
@@ -0,0 +1,12 @@
+#include "synthesis/tool.h"
+
+namespace gca {
+
+  std::ostream& operator<<(std::ostream& out, const tool& t) {
+    out << "Tool Number       = " << t.tool_number() << std::endl;
+    out << "Tool cut length   = " << t.cut_length() << std::endl;
+    out << "Tool shank length = " << t.shank_length() << std::endl;
+    return out;
+  }
+
+}
