fa0a60f67d8be203a11a5a9564933a061b57d723
Dillon Huff <dillonhuff@gmail.com>
2016-11-19 04:06:06 -0600

    Now parsing iges files into relevant sections

--- examples/parse_iges.cpp
+++ examples/parse_iges.cpp
@@ -42,18 +42,62 @@ std::vector<iges_record> lex_records(std::ifstream& iges_file) {
   return records;
 }
 
+struct iges_sections {
+  std::vector<iges_record> start;
+  std::vector<iges_record> global;
+  std::vector<iges_record> directory_entry;
+  std::vector<iges_record> parameter_data;
+  std::vector<iges_record> terminate;
+};
+
+iges_sections
+group_records_into_sections(const std::vector<iges_record>& records) {
+  vector<vector<iges_record> > sections;
+
+  vector<iges_record> current_section;
+  int current_no = 0;
+  for (auto& r : records) {
+    if (r.number == (current_no + 1)) {
+      current_section.push_back(r);
+      current_no = r.number;
+    } else if (r.number == 1) {
+      sections.push_back(current_section);
+      current_no = 1;
+      current_section = {r};
+    } else {
+      cout << "Bad record with number = " << r.number << endl;
+      cout << "Text = " << r.line << endl;
+      DBG_ASSERT(false);
+    }
+  }
+
+  sections.push_back(current_section);
+
+  cout << "# of sections = " << sections.size() << endl;
+
+  DBG_ASSERT(sections.size() == 5);
+  DBG_ASSERT(sections.back().size() == 1);
+
+  return iges_sections{sections[0],
+      sections[1],
+      sections[2],
+      sections[3],
+      sections[4]};
+}
+
 void parse_iges(const std::string& file_name) {
   ifstream iges_file(file_name.c_str());
 
   std::vector<iges_record> records = lex_records(iges_file);
 
   cout << "# of records = " << records.size() << endl;
-  for (auto& r : records) {
-    cout << "RECORD # = " << r.number << endl;
-    cout << "LINE     = " << r.line << endl;
-  }
+  // for (auto& r : records) {
+  //   cout << "RECORD # = " << r.number << endl;
+  //   cout << "LINE     = " << r.line << endl;
+  // }
 
-  
+  iges_sections sections =
+    group_records_into_sections(records);
 }
 
 int main(int argc, char* argv[]) {
