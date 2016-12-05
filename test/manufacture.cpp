f9483d87c0c01dbd3291432ac33051de0e3e8aeb
Dillon Huff <dillonhuff@gmail.com>
2016-12-04 20:39:14 -0800

    Removed Part 4 - 1 from manufacture tests since I am not actually going to cut it

--- test/manufacture.cpp
+++ test/manufacture.cpp
@@ -75,10 +75,9 @@ namespace gca {
     workpiece wp(1.75, 1.75, 2.5, ALUMINUM);
 
     vector<part_info> some_scaling{
-      {"test/stl-files/onshape_parts/PSU Mount - PSU Mount.stl", 1.0, wp},
-	{"test/stl-files/onshape_parts/Part Studio 1 - Part 1(29).stl", 0.5, wp},
-	  {"test/stl-files/OctagonWithHolesShort.stl", 1.0, wp},
-	    {"test/stl-files/onshape_parts/Part Studio 4 - Part 1.stl", 0.2, wp},
+	{"test/stl-files/onshape_parts/PSU Mount - PSU Mount.stl", 1.0, wp},
+	  {"test/stl-files/onshape_parts/Part Studio 1 - Part 1(29).stl", 0.5, wp},
+	    {"test/stl-files/OctagonWithHolesShort.stl", 1.0, wp},
 	      {"test/stl-files/onshape_parts/Japanese_Two_Contours_Part.stl", 0.45, wp},
 
 		{"test/stl-files/onshape_parts/Part Studio 1 - ESC spacer.stl", 0.65, wp},
