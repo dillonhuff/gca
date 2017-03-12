2f728910825abfa8a865942368f987d75b8c1a81
Dillon Huff <dillonhuff@gmail.com>
2017-03-11 22:18:48 -0800

    Now have primitive voxel volume with occupation testing and setting

--- src/geometry/voxel_volume.h
+++ src/geometry/voxel_volume.h
@@ -1,8 +1,31 @@
+#include <boost/container/flat_set.hpp>
+
 #include "geometry/point.h"
 
 namespace gca {
 
+  struct voxel {
+    int xi, yi, zi;
+  };
+
+  static inline bool operator<(const voxel l, const voxel r) {
+    if (l.xi < r.xi) { return true; }
+    if (l.xi > r.xi) { return false; }
+
+    if (l.yi < r.yi) { return true; }
+    if (l.yi > r.yi) { return false; }
+
+    if (l.zi < r.zi) { return true; }
+
+    return false;
+      
+  }
+
   class voxel_volume {
+
+    boost::container::flat_set<voxel> voxels;
+  
+    
   public:
     voxel_volume(const point origin,
 		 const double x_len,
@@ -15,11 +38,12 @@ namespace gca {
     }
 
     inline bool is_occupied(int x_i, int y_i, int z_i) const {
-      return false;
+      return voxels.find( voxel{x_i, y_i, z_i} ) != voxels.end();
     }
 
 
     inline void set_occupied(int x_i, int y_i, int z_i) {
+      voxels.insert( voxel{x_i, y_i, z_i} );
     }
     
   };
