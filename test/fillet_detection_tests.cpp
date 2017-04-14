ef2210d1398cc41871afe0c9ca03f8e9055b2232
Dillon Huff <dillonhuff@gmail.com>
2017-04-14 14:28:52 -0700

    Crude outline based detection actually works reasonably well

--- test/fillet_detection_tests.cpp
+++ test/fillet_detection_tests.cpp
@@ -3,20 +3,40 @@
 #include "feature_recognition/fillet_detection.h"
 #include "geometry/vtk_debug.h"
 #include "system/parse_stl.h"
+#include "utils/arena_allocator.h"
 
 namespace gca {
 
-  TEST_CASE("100-009 - Part 1.stl") {
-    auto mesh =
-      parse_stl("./test/stl-files/onshape_parts/100-009 - Part 1.stl", 0.0001);
+  TEST_CASE("Fillet detection") {
+    arena_allocator a;
+    set_system_allocator(&a);
+
+    SECTION("100-009 - Part 1.stl") {
+      auto mesh =
+	parse_stl("./test/stl-files/onshape_parts/100-009 - Part 1.stl", 0.0001);
 
 
-    vector<vector<surface> > fillets =
-      detect_fillets(mesh);
+      vector<vector<surface> > fillets =
+	detect_fillets(mesh);
 
-    visualize_surface_decomp(fillets);
+      visualize_surface_decomp(fillets);
 
-    REQUIRE(fillets.size() == 2);
+      REQUIRE(fillets.size() == 2);
+    }
+
+    SECTION("Japanese 2 contours") {
+      auto mesh =
+	parse_stl("./test/stl-files/onshape_parts/Japanese_Two_Contours_Part.stl",
+		  0.0001);
+
+      vector<vector<surface> > fillets =
+	detect_fillets(mesh);
+
+      visualize_surface_decomp(fillets);
+
+      REQUIRE(fillets.size() == 9);
+    }
+
   }
 
 }
