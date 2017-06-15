051bae3b4eeaa0947efdf4883ea8c8df91b7c23a
Dillon Huff <dillonhuff@gmail.com>
2017-06-15 15:39:10 -0700

    Looks like drilling is a large part of each step

--- test/drill_optimization_tests.cpp
+++ test/drill_optimization_tests.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 
 #include "analysis/gcode_to_cuts.h"
+#include "gcode/visual_debug.h"
 #include "utils/algorithm.h"
 
 using namespace std;
@@ -14,6 +15,14 @@ namespace gca {
     delete_if(drills, [](const cut* c) { return !c->is_linear_cut() || !is_vertical(c); });
 
     return drills;
+  }
+
+  double length(const std::vector<cut*>& cuts) {
+    double len = 0;
+    for (auto c : cuts) {
+      len += c->length();
+    }
+    return len;
   }
 
   TEST_CASE("Counting needed drilling actions") {
@@ -35,8 +44,15 @@ namespace gca {
 
     REQUIRE(paths.size() == 1);
 
+    vtk_debug_cuts(paths[0]);
+
+    double len = length(paths[0]);
+    cout << "total length = " << len << endl;
+    
     vector<cut*> drill_cuts =
       extract_drill_downs(paths[0]);
+
+    cout << "drill length = " << length(drill_cuts) << endl;
 
     REQUIRE(drill_cuts.size() == num_drills);
   }
