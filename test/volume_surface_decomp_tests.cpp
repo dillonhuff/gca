2b9a508ccb775929ede3b012d2f4e6c9ac6df7f0
Dillon Huff <dillonhuff@gmail.com>
2017-06-22 14:44:49 -0700

    Negative space computation works for all parts in test/stl-files/onshape_parts

--- test/volume_surface_decomp_tests.cpp
+++ test/volume_surface_decomp_tests.cpp
@@ -5,6 +5,7 @@
 #include "geometry/triangular_mesh_utils.h"
 #include "geometry/vtk_debug.h"
 #include "synthesis/visual_debug.h"
+#include "system/file.h"
 #include "system/parse_stl.h"
 
 namespace gca {
@@ -24,37 +25,44 @@ namespace gca {
 	"test/stl-files/onshape_parts/Japanese_Two_Contours_Part.stl",
 	"test/stl-files/onshape_parts/Part Studio 1 - Part 1.stl",
 	"test/stl-files/onshape_parts/Part Studio 1 - Falcon Prarie .177 single shot tray.stl"};
+
+  void compute_negative_space(const std::string& n) {
+    if (!ends_with(n, "stl")) {
+      return;
+    }
+
+    auto mesh = parse_stl(n, 0.0001);
+
+    box bb = mesh.bounding_box();
+    double eps = 0.000001;
+    bb.x_min += eps;
+    bb.y_min += eps;
+    bb.z_min += eps;
+
+    bb.x_max -= eps;
+    bb.y_max -= eps;
+    bb.z_max -= eps;
+
+    triangular_mesh stock = make_mesh(box_triangles(bb), 0.001);
+
+    //vtk_debug_meshes({mesh, stock});
+
+    Nef_polyhedron mesh_nef = trimesh_to_nef_polyhedron(mesh);
+    Nef_polyhedron stock_nef = trimesh_to_nef_polyhedron(stock);
+    auto negative_space = stock_nef - mesh_nef;
+
+    cout << "about to convert to trimeshes" << endl;
+    auto neg_meshes = nef_polyhedron_to_trimeshes(negative_space);
+
+    vtk_debug_meshes(neg_meshes);
+    
+  }
   
   TEST_CASE("Subtracting from ") {
     // auto mesh =
     //   parse_stl("./test/stl-files/onshape_parts/100-009 - Part 1.stl", 0.0001);
 
-    for (auto m_str : test_parts) {
-      auto mesh = parse_stl(m_str, 0.0001);
-
-      box bb = mesh.bounding_box();
-      double eps = 0.000001;
-      bb.x_min += eps;
-      bb.y_min += eps;
-      bb.z_min += eps;
-
-      bb.x_max -= eps;
-      bb.y_max -= eps;
-      bb.z_max -= eps;
-
-      triangular_mesh stock = make_mesh(box_triangles(bb), 0.001);
-
-      //vtk_debug_meshes({mesh, stock});
-
-      Nef_polyhedron mesh_nef = trimesh_to_nef_polyhedron(mesh);
-      Nef_polyhedron stock_nef = trimesh_to_nef_polyhedron(stock);
-      auto negative_space = stock_nef - mesh_nef;
-
-      cout << "about to convert to trimeshes" << endl;
-      auto neg_meshes = nef_polyhedron_to_trimeshes(negative_space);
-
-      vtk_debug_meshes(neg_meshes);
-    }
+    read_dir("./test/stl-files/onshape_parts/", compute_negative_space, ".stl");
   }
 
 }
