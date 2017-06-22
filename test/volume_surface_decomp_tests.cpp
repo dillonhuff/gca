5fa3489c538933861598767b1eace34e31d255f7
Dillon Huff <dillonhuff@gmail.com>
2017-06-22 12:25:18 -0700

    With 1e-6 offset of bounding box the negative volume for one part looks ok

--- test/volume_surface_decomp_tests.cpp
+++ test/volume_surface_decomp_tests.cpp
@@ -12,7 +12,29 @@ namespace gca {
   TEST_CASE("Subtracting from ") {
     auto mesh =
       parse_stl("./test/stl-files/onshape_parts/100-009 - Part 1.stl", 0.0001);
-    REQUIRE(false);
+
+    box bb = mesh.bounding_box();
+    double eps = 0.000001;
+    bb.x_min += eps;
+    bb.y_min += eps;
+    bb.z_min += eps;
+
+    bb.x_max -= eps;
+    bb.y_max -= eps;
+    bb.z_max -= eps;
+
+    triangular_mesh stock = make_mesh(box_triangles(bb), 0.001);
+
+    //vtk_debug_meshes({mesh, stock});
+
+    Nef_polyhedron mesh_nef = trimesh_to_nef_polyhedron(mesh);
+    Nef_polyhedron stock_nef = trimesh_to_nef_polyhedron(stock);
+    auto negative_space = stock_nef - mesh_nef;
+
+    cout << "about to convert to trimeshes" << endl;
+    auto neg_meshes = nef_polyhedron_to_trimeshes(negative_space);
+
+    vtk_debug_meshes(neg_meshes);
   }
 
 }
