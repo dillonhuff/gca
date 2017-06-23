ed7161c48e50e2b155c08df3c4380548fda53842
Dillon Huff <dillonhuff@gmail.com>
2017-06-22 20:39:15 -0700

    With nef unions the result actually looks pretty good

--- test/volume_surface_decomp_tests.cpp
+++ test/volume_surface_decomp_tests.cpp
@@ -27,7 +27,7 @@ namespace gca {
 	"test/stl-files/onshape_parts/Part Studio 1 - Part 1.stl",
 	"test/stl-files/onshape_parts/Part Studio 1 - Falcon Prarie .177 single shot tray.stl"};
 
-  Nef_polyhedron compute_negative_space(const triangular_mesh& mesh) {
+  box eps_adjusted_bounding_box(const triangular_mesh& mesh) {
     box bb = mesh.bounding_box();
     double eps = 0.000001;
     bb.x_min += eps;
@@ -37,6 +37,20 @@ namespace gca {
     bb.x_max -= eps;
     bb.y_max -= eps;
     bb.z_max -= eps;
+
+    return bb;
+  }
+
+  Nef_polyhedron compute_negative_space(const triangular_mesh& mesh) {
+    box bb = eps_adjusted_bounding_box(mesh);
+    // double eps = 0.000001;
+    // bb.x_min += eps;
+    // bb.y_min += eps;
+    // bb.z_min += eps;
+
+    // bb.x_max -= eps;
+    // bb.y_max -= eps;
+    // bb.z_max -= eps;
 
     triangular_mesh stock = make_mesh(box_triangles(bb), 0.001);
 
@@ -82,10 +96,24 @@ namespace gca {
 
     vtk_debug_meshes(mvs_meshes);
 
-    auto negative_space = compute_negative_space(mesh);
+    box bb = eps_adjusted_bounding_box(mesh);
+
+    triangular_mesh stock = make_mesh(box_triangles(bb), 0.001);
+
+    Nef_polyhedron mesh_nef = trimesh_to_nef_polyhedron(mesh);
+    for (auto& mv : mvs_meshes) {
+      mesh_nef = mesh_nef + trimesh_to_nef_polyhedron(mv);
+    }
+    Nef_polyhedron stock_nef = trimesh_to_nef_polyhedron(stock);
+    auto negative_space = stock_nef - mesh_nef;
+    
+    //auto negative_space = compute_negative_space(mesh);
+    
 
     cout << "about to convert to trimeshes" << endl;
     auto neg_meshes = nef_polyhedron_to_trimeshes(negative_space);
+
+    vtk_debug_meshes(neg_meshes);
   }
 
 }
